1d494eb1bc0b76f4fdb0bc49ed4471b0dfaefa44
joehiggi <joehiggi@umich.edu>
2025-10-27 16:04:46 +0000

    updated files

--- src/docker/install_0.sh
+++ src/docker/install_0.sh
@@ -4,4 +4,4 @@ set -e
 yes | apt install vim && \
 python3 -m venv .env && \
 source .env/bin/activate && \
-python3 -m pip install -r /workspace/requirements.txt --break-system-packages
+pip install -r /workspace/requirements.txt --break-system-packages
