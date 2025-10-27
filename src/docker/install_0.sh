cb3c06abbd148c8ca1c77db62ae62857b1050468
joehiggi <126930963+joehiggi@users.noreply.github.com>
2025-10-26 23:58:04 -0400

    Update install_0.sh

--- src/docker/install_0.sh
+++ src/docker/install_0.sh
@@ -3,5 +3,5 @@ set -e
 
 yes | apt install vim && \
 python3 -m venv .env && \
-bash .env/bin/activate && \
+source .env/bin/activate && \
 python3 -m pip install -r /workspace/requirements.txt --break-system-packages
